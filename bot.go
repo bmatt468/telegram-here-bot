bb1fcbf0ffdd4f622bb7c31fde68342c05409805
bmatt468 <benjamin@benjaminrmatthews.com>
2017-03-07 18:04:02 -0500

    testing db connection

--- bot.go
+++ bot.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+    "database/sql"
 	"log"
+    "github.com/go-sql-driver/mysql"
 	"gopkg.in/telegram-bot-api.v4"
 	"net/http"
     "strings"
@@ -36,15 +38,83 @@ func main() {
     for update := range updates {
         if update.Message != nil {
             switch cmd := strings.Split(update.Message.Text, " "); strings.Replace(cmd[0],"@everyone_here_bot","",-1) {
-                case "/start":
-                    log.Printf("%+v\n", cmd)
-                case "/register":
-                    log.Printf("%+v\n", cmd)
-                case "/deregister":
-                    log.Printf("%+v\n", cmd)
-                case "/all", "/here":
-                    log.Printf("%+v\n", cmd)
+            case "/start":
+                msg := tgbotapi.NewMessage(update.Message.Chat.ID, "HereBot is now active.")
+                bot.Send(msg)
+
+            case "/register":
+                log.Printf("%+v\n", cmd)
+
+            case "/deregister":
+                // Connect to our database
+                db, err := ConnectDB()
+                if err != nil {
+                    log.Fatal(err)
+                }
+                defer db.Close()
+
+                if len(cmd) == 1 || len(cmd) > 2 {
+                    // Wrong Usage
+                    msg_string := "Correct Usage: /deregister @username"
+                    msg := tgbotapi.NewMessage(update.Message.Chat.ID, msg_string)
+                    bot.Send(msg)
+
+                } else if cmd[1] != "@"+update.Message.From.UserName {
+                    msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You may not deregister another user.")
+                    bot.Send(msg)
+                } else if len(cmd) == 2 {
+                    result, err := db.Query("UPDATE users SET flag_active=0 WHERE chat_id=? AND username=? ",update.Message.Chat.ID,cmd[1])
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+                    defer result.Close()
+
+                    msg_string := cmd[1]+" has been deregistered."
+                    msg := tgbotapi.NewMessage(update.Message.Chat.ID, msg_string)
+                    bot.Send(msg)
+                }
+
+            case "/all", "/here":
+                db, err := ConnectDB()
+                if err != nil {
+                    log.Fatal(err)
+                }
+                defer db.Close()
+
+                users, err := db.Query("SELECT username FROM users u WHERE chat_id=? AND flag_active=1",update.Message.Chat.ID)
+                if err != nil {
+                    log.Fatal(err)
+                }
+                defer users.Close()
+
+                msg_string := ""
+
+                for users.Next() {
+                    var username string
+                    if err := users.Scan(&username); err != nil {
+                        log.Fatal(err)
+                    }
+                    msg_string += " " + username
+                }
+                if err := users.Err(); err != nil {
+                        log.Fatal(err)
+                }
+
+                msg := tgbotapi.NewMessage(update.Message.Chat.ID, msg_string)
+                bot.Send(msg)
             }
         }
     }
 }
+
+func ConnectDB() (*sql.DB, error) {
+    cfg := &mysql.Config {
+        User: "hello-bot",
+        Passwd: "H3Ll0B0T",
+        Net: "tcp",
+        Addr: "107.170.45.12:3306",
+        DBName: "hello-bot",
+    }
+    return sql.Open("mysql", cfg.FormatDSN())
+}
+
